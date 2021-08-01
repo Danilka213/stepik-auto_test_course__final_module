805221580d73c596baa63d1e935dadc23aa1e149
Danil Khudzhev <hygdeq@gmail.com>
2021-08-01 12:24:20 +0300

    Finally commit, should be cods

--- test_product_page.py
+++ test_product_page.py
@@ -19,7 +19,8 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.register_new_user()
         page.should_be_authorized_user()
-        
+      
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
         page = ProductPage(browser, link)
@@ -31,17 +32,22 @@ class TestUserAddToBasketFromProductPage():
         page = ProductPage(browser, link)
         page.open()
         page.should_be_not_visible_message_add_product_in_backet(*ProductPageLocators.MESSAGE_ADD_PRODUCT_IN_BACKET)
-    
-"""
-    
+        
+@pytest.mark.need_review    
+def test_quest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+        
 @pytest.mark.parametrize('links', ["0", "1","2","3","4","5","6" , pytest.param("7", marks=pytest.mark.xfail),"8","9" ])
 def test_guest_can_add_product_to_basket(browser, links):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{links}"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_login_page()"""
+    page.should_be_login_page()
    
-"""@pytest.mark.xfail   
+@pytest.mark.xfail   
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
     page = ProductPage(browser, link)
@@ -49,7 +55,11 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.add_product_in_backet()
     page.should_be_not_visible_message_add_product_in_backet(*ProductPageLocators.MESSAGE_ADD_PRODUCT_IN_BACKET)
  
-
+def test_quest_cant_see_success_message(browser): 
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_not_visible_message_add_product_in_backet(*ProductPageLocators.MESSAGE_ADD_PRODUCT_IN_BACKET)
 
  
 @pytest.mark.xfail
@@ -66,16 +76,18 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-    
+
+@pytest.mark.need_review    
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page(*BasePageLocators.LOGIN_LINK)
     login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()"""
+    login_page.should_be_login_page()
     
-"""def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+@pytest.mark.need_review    
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
@@ -90,4 +102,4 @@ def test_guest_can_see_product_in_basket_opened_from_product_page(browser):
     page.open()
     page.go_to_login_page(*BasketPageLocators.BASKET_BUTTON)
     basket_page = BasketPage(browser, browser.current_url)
-    basket_page.should_be_filled_basket()"""
+    basket_page.should_be_filled_basket()
